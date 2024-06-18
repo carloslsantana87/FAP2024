fc28db66c6ada2b18e92e0535d9402a709b2863a
carloslsantana87 <carloslsantana87@gmail.com>
2024-06-18 15:01:08 -0300

    primeiro commit

---
+++ logica/aula1.js
@@ -0,0 +1,8 @@
+
+var a = 3;
+var b = 10;
+var c = 25;
+
+var soma = a + b + c;
+
+console.log("A soma dos três números é " + soma);
---
+++ logica/aula2.js
@@ -0,0 +1,18 @@
+var nota1 = 10;
+var nota2 = 10;
+var nota3 = 8;
+var nota4 = 9;
+var nota5 = 10;
+
+var media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+console.log("A média das notas é " + media);
+
+var a = 10;
+var b = 2;
+var c = 5;
+var d = 3;
+
+var result = a * b + c - d * b - a / b;
+
+console.log("O resultado da expressão é " + result);
